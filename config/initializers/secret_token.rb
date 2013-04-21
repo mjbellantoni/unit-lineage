977ef05121ed42eab9bbb7b80c4520072db82132
Matthew Bellantoni <mjbellantoni@yahoo.com>
2013-04-21 12:38:31 -0400

    Adds a basic welcome page.
    . Add welcome#index.
    . Set up basic layout using Bootstrap.
    . Set up RSpec.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnitLineage::Application.config.secret_token = 'fddf557b5232cafe9cb7a104b6c5fb9df0b3e5caf5a25501fb4fb45d2f05e5627ce6b24bdc8a4f6ef04ac16c70d05a111707fb82ddae7e0852978dfc7dc63fdb'
